6b2b71b031e4fbf9b2882771c8a1538bd02245b8
ruthreshjp <ruthreshprabhakaran@gmail.com>
2026-07-02 16:15:22 +0530

    additional hands-on Implementing constructor and setter Injection

---
+++ 06)SpringCoreAndMaven/Additional_Hands-on/ImplementingConstructorAndSetterInjection/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,24 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Constructor Injection
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Constructor Injection Successful");
+    }
+
+    // Setter Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Setter Injection Successful");
+    }
+
+    public void show() {
+        bookRepository.display();
+    }
+}

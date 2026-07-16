08dd62ae3b4cc9a937669167f3ec4e4c74b167c9
ruthreshjp <ruthreshprabhakaran@gmail.com>
2026-07-16 14:29:11 +0530

    blogapp

---
+++ 10)React/React_4/blogapp/src/Posts.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+    loadPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({
+                    posts: data
+                });
+            });
+    }
+    componentDidMount() {
+        this.loadPosts();
+    }
+    render() {
+         return (
+            <div>
+                <h1>Blog Posts</h1>
+
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+    componentDidCatch(error, info){
+        alert('An error occurred while loading posts');
+    }
+}
+export default Posts;

36eb74e1a8d03402e1a4b8f8e70b032a5ad89673
Ruthresh JP <ruthreshprabhakaran@gmail.com>
2026-07-22 16:01:35 +0530

    Spring data JPA additional hands on 2

---
+++ 07)SpringDataJPA/Additional_Handson/02)FindCountryBasedOnCountryCode/country-service/src/main/java/com/example/countryservice/service/exception/CountryNotFoundException.java
@@ -0,0 +1,8 @@
+package main.java.com.example.countryservice.service.exception;
+
+public class CountryNotFoundException extends RuntimeException {
+
+    public CountryNotFoundException(String message) {
+        super(message);
+    }
+}
